6e082679b1bdc49ea9e4bc2ca58762bc1260971d
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-07 14:29:45 -0500

    initial tailwind in App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App() {
     return (
-        <h1>Hello, world!</h1>
+        <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap">
+            <h2 className="text-4xl text-yellow-500">Hello, world!</h2>
+        </div>
     );
 }
