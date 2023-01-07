b627a9fab66738603f361efb1d415229dbcdb686
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-07 14:41:16 -0500

    add mock data to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { User } from "./types";
+
 export default function App() {
+    const users: User[] = [
+        {
+            name: "Dominique",
+            messages: [
+                {
+                    body: "Prisma rocks!",
+                },
+                {
+                    body: "Did I mention I love Prisma?",
+                },
+            ],
+        },
+];
+
     return (
         <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap">
             <h2 className="text-4xl text-yellow-500">Hello, world!</h2>
