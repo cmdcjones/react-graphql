04e6588232c1e6a22c07b44f059a1045f7292490
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-09 19:56:19 -0500

    refactor context according to apollo server docs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema } from "./graphql/schema.js";
+import { context } from "./context.js";
 import { PORT } from "./utils.js";
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ 
+    schema,
+});
 
 const { url } = await startStandaloneServer(server, {
     listen: { port: PORT },
+    context: async () => context,
 });
 
 console.log(`Server is ready at ${url}`);
