4795526f31c60720f637ac9fba239f037fc7d512
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-09 19:14:31 -0500

    create index for graphql types; import in schema

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -3,11 +3,13 @@ import { makeSchema } from "nexus";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
+import * as types from ".";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const schema = makeSchema({
-    types: [],
+    types,
     outputs: {
         schema: join(__dirname, '..', 'schema.graphql'),
         typegen: join(__dirname, '..', 'nexus-typegen.ts'),
