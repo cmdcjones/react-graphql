5be4c5354081848783379fb956310b65dd304c40
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-09 19:36:13 -0500

    add MessageQuery; extends Query

--- server/src/graphql/Message.ts
+++ server/src/graphql/Message.ts
@@ -1,9 +1,31 @@
-import { objectType } from "nexus";
+import { extendType, objectType } from "nexus";
 
 export const Message = objectType({
     name: "Message",
     definition(t) {
         t.int("id");
         t.string("body");
+    },
+});
+
+export const MessageQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.list.field("hello", {
+            type: "Message",
+
+            resolve () {
+                return [
+                    {
+                        id: 1,
+                        body: "hello there",
+                    },
+                    {
+                        id: 2,
+                        body: "goodbye here",
+                    },
+                ];
+            },
+        });
     },
 });
