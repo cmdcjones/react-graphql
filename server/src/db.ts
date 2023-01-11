e3107ba019fec841b3efe4af1a9d2fbef25527bc
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-11 16:07:39 -0500

    add User type to index; add User interface to db

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,13 +5,20 @@ export interface Message {
     body: string
 }
 
+export interface User {
+    id: number
+    name: string
+}
+
 // How the data in Db should be formatted, accepted, and interacted with
 export interface Db {
     messages: Message[]
+    users: User[]
 }
 
 // Export a 'database' that contains an accepted field 'messages' that is 
 // an array containing data of type Message
 export const db: Db = {
     messages: [{ id: 1, body: "hello there", }, { id: 2, body: "goodbye here", }],
+    users: [{ id: 1, name: "Dom" }, { id: 2, name: "Test" }],
 };
--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,2 +1,3 @@
 // export all types to be used by GraphQL
 export * from "./Message.js";
+export * from "./User.js";
